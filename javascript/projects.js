a5b2b816f5d8f6434e80632ecdcf6bc5d8771c19
Aleksander P <77441538+ManInBlack-coder@users.noreply.github.com>
2025-02-16 16:08:22 +0200

    completed projektide õiged kirjeldused task

--- javascript/projects.js
+++ javascript/projects.js
@@ -18,16 +18,16 @@ function openPopup(projectId) {
         Private = 'true';
         projectName = 'InternHub';
         projectImage = 'images/Internhub.webp';
-        projectDescription1 = 'A platform to connect interns with real-world opportunities in tech companies. It offers an easy-to-use interface for both companies and interns to create profiles, search for opportunities, and apply for jobs.';
-        projectDescription2 = 'With features like job notifications, detailed company profiles, and an application tracker, InternHub helps bridge the gap between talent and tech companies. It\'s a powerful tool for interns to kickstart their careers in the tech industry.';
+        projectDescription1 = 'A platform that connects intern with companies and prepares them for upcoming internship. Platform includes internships matching, artificial intelligence powered interview simulator and preparation tasks that are helping to understand how working really is in companies .';
+        projectDescription2 = 'InternHub startup was founded by me and my classmates since we won II place at EdTech Hackaton 2024. By myself in this project now I am responsible of platforms AI powered interview simulator developement and supporting other frontend components developements.';
         projectLink = 'https://internhub.arendusekoobas.ee/';
         githubLink = ''; // No GitHub link for private projects
     } else if (projectId === 'anyhouseradio') {
         Private = 'false';
         projectName = 'Any House Radio';
-        projectImage = 'images/anyhouseradio.webp';
-        projectDescription1 = 'House music radio show hosted by DJ/Producer Eurlexa. The show features live mixes, guest appearances, and the best in house and electronic music. Tune in for a non-stop party vibe.';
-        projectDescription2 = 'Any House Radio offers an immersive listening experience, allowing fans to discover fresh tracks, legendary classics, and interviews with top DJs and producers. It\'s the ultimate destination for house music lovers.';
+        projectImage = 'images/anyhouseradio.webp','images/Tstock.webp';
+        projectDescription1 = 'Official website of House music radio podcast show that is hosted by DJ/Producer Eurlexa.';
+        projectDescription2 = 'Website was created for sharing latest episodes, remix releases and news of Any House Radio. Any House Radio is With little underground subtone radio podcast that offers around hour long mix episodes and remix releases to people who wants hear energetic and cosily house tunes.';
         projectLink = 'https://anyhouseradio.com';
         githubLink = 'https://github.com/ManInBlack-coder/any_house_radio'; // Example GitHub link
         githubLogo = 'images/githublogo.webp'; // GitHub logo for public projects
@@ -35,53 +35,36 @@ function openPopup(projectId) {
         Private = 'true';
         projectName = 'ThousandStock';
         projectImage = 'images/Tstock.webp';
-        projectDescription1 = 'ThousandStock is a stock analysis and management platform designed to help investors make better decisions. The platform tracks stock trends, provides analysis on potential investments, and offers tools for portfolio management.';
-        projectDescription2 = 'By analyzing real-time data and providing insightful metrics, ThousandStock helps users identify profitable opportunities and manage their investments with ease. Whether you\'re a novice or an experienced investor, ThousandStock can guide you to smarter financial decisions.';
+        projectDescription1 = 'ThousandStock is dividend shares oriented stock managament and monitoring platform.';
+        projectDescription2 = 'This Platform idea is to automatically monitore best stocks that are paying most dividend money. Platform also let\'s manage information and articles of stocks.';
         projectLink = '#';
         githubLink = ''; // No GitHub link for private projects
     } else if (projectId === 'callassistant') {
         Private = 'true';
         projectName = 'Call Assistent';
         projectImage = 'images/Callast.webp';
-        projectDescription1 = 'Call Assistent is a real-time voice recognition application designed to convert speech into text. Ideal for professionals, students, and anyone in need of hands-free note-taking, this app ensures accuracy and efficiency.';
-        projectDescription2 = 'The app utilizes advanced speech-to-text technology to transcribe conversations, meetings, or lectures in real-time. Users can easily save, edit, and share the transcriptions for later use.';
+        projectDescription1 = 'Call Assistent is fast notes managament application that let\'s user record his/her tasks to app.';
+        projectDescription2 = 'The app has speechrecognition technology that makes tood adding usage faster.';
         projectLink = '#';
         githubLink = ''; // No GitHub link for private projects
-    } else if (projectId === 'estparty') {
-        Private = 'false';
-        projectName = 'EstpartyPool';
-        projectImage = 'images/estparty.webp';
-        projectDescription1 = 'Estparty is an event management platform designed to help users organize and promote local parties and events. From creating event listings to selling tickets, Estparty provides all the tools you need for a successful event.';
-        projectDescription2 = 'With features like guest list management, ticket tracking, and event promotion tools, Estparty is the ultimate solution for event organizers looking to streamline their event planning and increase attendance.';
-        projectLink = 'https://aleksanderita22.ikt.khk.ee/Veebiarendus/Mitmeleheline/project/Index.html';
-        githubLink = 'https://github.com/ManInBlack-coder/estparty_nostalgiline_essa_veebisait'; // Example GitHub link
-        githubLogo = 'images/githublogo.webp'; // GitHub logo for public projects
     } else if (projectId === 'youtube') {
         Private = 'true';
         projectName = 'YouTube mp4 to Wav';
         projectImage = 'images/YOUTUBE.webp';
-        projectDescription1 = 'This project is a clone of YouTube designed to practice frontend development skills. The app allows users to view videos, listen to music, and convert YouTube mp4 videos to wav audio format for offline listening.';
-        projectDescription2 = 'Incorporating video streaming, playlist creation, and audio conversion functionality, this project demonstrates the power of frontend technologies for building interactive and useful web applications.';
+        projectDescription1 = 'Youtube video to WAV audio downloader that is created for educational purposes';
+        projectDescription2 = 'This simple web app was created for faster, easier and with more quality no copyright sound music downloading. It saves download history that allows download again song when it\'s gone missing or something else has happened';
         projectLink = '#';
         githubLink = ''; // No GitHub link for private projects
     } else if (projectId === 'carsearch') {
         Private = 'false';
         projectName = 'Car search database';
         projectImage = 'images/auto24.webp';
-        projectDescription1 = 'Car Search Database is an automotive marketplace platform where users can search for cars, buy and sell vehicles, and compare prices from various sellers. The platform provides detailed car listings, search filters, and price comparisons.';
-        projectDescription2 = 'Whether you\'re buying your first car or selling a vehicle, Car Search Database makes it easier to find the perfect deal with comprehensive details about each listing and access to a wide selection of vehicles.';
+        projectDescription1 = 'Car Search Database that search logic is almost identical with Auto24 car searching engine.';
+        projectDescription2 = 'This car search database web solution was created for practising php usage in web projects. That gave me better understanding of using php for serverside problem solutions.';
         projectLink = '#';
         githubLink = 'https://github.com/ManInBlack-coder/auto24_PHP'; // Example GitHub link
         githubLogo = 'images/githublogo.webp'; // GitHub logo for public projects
-    } else if (projectId === 'aiinterview') {
-        Private = 'true';
-        projectName = 'AI Interview Simulator';
-        projectImage = 'images/ai.webp';
-        projectDescription1 = 'AI Interview leverages AI-powered solutions to enhance automation and data analysis capabilities. It is designed to simulate real interview scenarios and assess candidates based on their responses, helping employers make more informed hiring decisions.';
-        projectDescription2 = 'The system evaluates candidate responses using natural language processing and provides feedback on areas such as communication skills, problem-solving, and technical knowledge.';
-        projectLink = '#';
-        githubLink = ''; // No GitHub link for private projects
-    }
+    } 
 
     // Display private repository message if no GitHub link
     if (!githubLink) {
